d64cb24db5d9d762e9c536d275efe8a4fda6ec3f
Sriya Bhat <sriya.s.bhat@gmail.com>
2020-09-15 03:44:16 +0000

    Compare if the Two Values are Equal : Customized assert function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual,expected) {
+  let returnString = "";
+  if (actual === expected) {
+    returnString = " 😍 Assertion Passed: " + actual + " === " + expected;
+  } else {
+    returnString = " ☹ Assertion Failed: " + actual + " !== " + expected;
+  }
+  return returnString;
+};
+
+console.log(assertEqual("LightHouse Labs","Bootcamp"));
+console.log(assertEqual(1,3));
+console.log(assertEqual(12,12));
+console.log(assertEqual("Bootcamp","Bootcamp"));
