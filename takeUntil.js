f5ced76ff98f1852d4cc9422c29576812561f3e1
Sriya Bhat <sriya.s.bhat@gmail.com>
2020-09-19 17:33:13 +0000

    Implemented a higherorder function that will slice the array till the callback function returns true

---
+++ takeUntil.js
@@ -0,0 +1,58 @@
+const eqArrays = function(firstArray,secondArray) {
+  let matchedCount = 0;
+
+  //If both the arrays dont have the same number of elements then they are definitely not identical
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  } else {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] === secondArray[i]) {
+        matchedCount++;
+      } else {
+        break;
+      }
+    }
+    
+//If all the elements are equal then the matched count will be equal to the length of the array. 
+    if (matchedCount === firstArray.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+};
+const assertArraysEqual = function(firstArray,secondArray) {
+
+  if (eqArrays(firstArray,secondArray)) {
+    console.log(` 😍 Assertion Passed: ${firstArray}  === ${secondArray}`);
+  } else {
+    console.log(` ☹ Assertion Failed: ${firstArray}  === ${secondArray}`);
+  }
+};
+
+
+
+const takeUntil = function(array, callback) {
+  const results = [];
+
+  for (let item of array) {
+    if (callback(item)) {
+      return results;
+    } else {
+      results.push(item);
+    }
+  }
+   
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1,[1,2,5,7,2]);
+
+console.log('------------------------------------------------------------');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2,["I've","been","to","Hollywood"]);
